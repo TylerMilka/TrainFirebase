10ec77dceca16fc2310367b27cbafb4767d8262c
Tyler <tgmilka@gmail.com>
2016-12-09 15:51:22 -0600

    Diverted to Train Project

---
+++ app.js
@@ -0,0 +1,12 @@
+   var config = {
+    apiKey: "AIzaSyBeAG4anjql7qVrH9RIyk2olaGLMNc6unE",
+    authDomain: "fir-trainproject.firebaseapp.com",
+    databaseURL: "https://fir-trainproject.firebaseio.com",
+    storageBucket: "fir-trainproject.appspot.com",
+    messagingSenderId: "999700127736"
+  };
+
+
+ var database = firebase.database();
+
+  
